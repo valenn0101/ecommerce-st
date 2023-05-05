1770a3c5a593fd42fc04b01ac2d719b9afa08a96
Valentin <105892117+valenn0101@users.noreply.github.com>
2023-05-05 17:32:58 -0300

    Update layout.jsx

--- ecommerce-storydots/src/app/layout.jsx
+++ ecommerce-storydots/src/app/layout.jsx
@@ -5,10 +5,6 @@ import React from "react";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 
-export const metadata = {
-  title: "Ecommerce - StoryDots",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
