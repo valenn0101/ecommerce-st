b43ac792edf99ec5ae5dc98d409d8e5529434f06
valenn0101 <veroncaceresvalentin@gmail.com>
2023-05-05 17:04:11 -0300

    Login Page

--- ecommerce-storydots/src/app/page.js
+++ ecommerce-storydots/src/app/page.js
@@ -1,17 +1,48 @@
-"use client"
-import React, { useState, useEffect } from "react";
+"use client";
+import React, { useState } from "react";
 import "../styles/home.css";
+import axios from "axios";
+import Cookies from "js-cookie";
 import Loading from "@/components/Loading/Loading";
 
-import { useAuthentication } from "@/utils/auth";
+const Auth = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-function App() {
-  const { username, setUsername, password, setPassword, loading, error, handleLogin } = useAuthentication();
-  const [isClient, setIsClient] = useState(false);
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        "https://ecommerce-api-rest.onrender.com/api/v1/crud/auth",
+        {
+          username,
+          password,
+        }
+      );
+
+      const sessionId = response.data.sessionId;
+      Cookies.set("sessionId", sessionId);
+      console.log("Autenticación exitosa");
+      window.location.href = "/home";
+    } catch (error) {
+      console.error("Error de autenticación:", error);
+      setError(true);
+    }
+
+    setLoading(false);
+  };
 
   if (loading) {
     return <Loading />;
@@ -31,7 +62,7 @@ function App() {
             name="username"
             placeholder="login"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={handleUsernameChange}
           />
           <input
             type="password"
@@ -39,18 +70,15 @@ function App() {
             name="password"
             placeholder="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handlePasswordChange}
           />
           <input type="submit" className="fadeIn fourth" value="Log In" />
         </form>
 
         <div id="formFooter">
-          {isClient && (
-            <a className="underlineHover" href="/home">
-              Client? Click here.
-            </a>
-          )}
-
+          <a className="underlineHover" href="/home">
+            Client? Click here.
+          </a>
           {error && (
             <p className="error">Error: Please check your user or password</p>
           )}
@@ -58,6 +86,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Auth;
